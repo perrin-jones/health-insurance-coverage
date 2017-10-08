81fa9825a7fae35a856d6d90afd52b36c6fa368b
Perrin Jones <p.jones@columbia.edu>
2017-10-08 13:52:57 -0400

    Displays found address or asks user to re-enter

--- Part_2/script.js
+++ Part_2/script.js
@@ -1,6 +1,6 @@
 var KEY = 'f36d09f45b99bbd15ea79fc4b85882d97d1a44a3';
 
-function getAddress(ad1, ad2, ct, st, zp){
+function getAddress(ad1, ct, st, zp){
  $.ajax({
      url: "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?format=jsonp",
     dataType: "jsonp",
@@ -9,22 +9,23 @@ function getAddress(ad1, ad2, ct, st, zp){
        benchmark : "Public_AR_Census2010",
        vintage : "Census2010_Census2010",
        layers: "14",
-       address: ad1 + ct + st + zp
+       address: ad1 + " ," + ct + " ," + st + " ," + zp
     },
     success: function(response){
       console.log(response);
-     $("#address").text("success");
+     $("#address").text(response.result.addressMatches[0].matchedAddress);
     },
     error: function(response) {
       console.log(response);
-     $("#address").text("error");
+     $("#address").text("Address Not Found: Please re-enter address");
     }
  });
 }
 
+
+
 $(document).ready(function() {
   document.getElementById('submit-form').addEventListener('click', function (e) {
-    alert("it worked");
     e.preventDefault(); //prevent a submit button from submitting a form.
     getAddress(document.getElementById('address-1').value, document.getElementById('city').value,   
                document.getElementById('state').value, document.getElementById('postcode').value);
