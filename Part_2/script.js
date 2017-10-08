9a83a9b90ce61ce24d697f4b41f125628534f7ea
Perrin Jones <p.jones@columbia.edu>
2017-10-08 13:22:18 -0400

    CORS error has been solved

--- Part_2/script.js
+++ Part_2/script.js
@@ -2,16 +2,14 @@ var KEY = 'f36d09f45b99bbd15ea79fc4b85882d97d1a44a3';
 
 function getAddress(ad1, ad2, ct, st, zp){
  $.ajax({
-    url: "https://geocoding.geo.census.gov/geocoder/geographies/address",
+     url: "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?format=jsonp",
+    dataType: "jsonp",
     data: {
        format: "json",
        benchmark : "Public_AR_Census2010",
        vintage : "Census2010_Census2010",
        layers: "14",
-       street: ad1,
-       city: ct,
-       state: st,
-       zip: zp
+       address: ad1 + ct + st + zp
     },
     success: function(response){
       console.log(response);
@@ -28,8 +26,7 @@ $(document).ready(function() {
   document.getElementById('submit-form').addEventListener('click', function (e) {
     alert("it worked");
     e.preventDefault(); //prevent a submit button from submitting a form.
-    getAddress(document.getElementById('address-1').value, document.getElementById('address-2').value,
-              document.getElementById('city').value, document.getElementById('state').value, 
-              document.getElementById('postcode').value);
-});
+    getAddress(document.getElementById('address-1').value, document.getElementById('city').value,   
+               document.getElementById('state').value, document.getElementById('postcode').value);
+  });
 });
